857ccd6b2fbccdfb749cb6fd4da00b02abdcbbaf
dengjunpeng <670124965@qq.com>
2019-10-18 17:33:40 +0800

    创建视图函数login

--- views.py
+++ views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcut import redirect
 
 def index(request):
     return HttpResponse('index')
+
+def login(request):
+    return redirect('/index')
 
